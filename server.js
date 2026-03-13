cd6f3e5af2ee123ab175bfa2f1bd65d767e5ee7b
mmill211 <mmill2111@westada.org>
2026-03-13 14:09:55 +0000

    Added pages and navigation

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const app = express()
  
 const PORT = 3000
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to My Website</h1>")
-})
-
-app.get('/about', (req, res) => {
-    res.send("<h1>About Me</h1><p>this page describes me</p>")
-})
+app.use(express.static("public"))
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
