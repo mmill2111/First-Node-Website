45d117f2e50ef6ae71c865877abf604f96409745
mmill211 <mmill2111@westada.org>
2026-03-24 14:37:21 +0000

    finished hamburger menu

--- public/navbar.js
+++ public/navbar.js
@@ -1,3 +1,7 @@
 function toggleMenu() {
     document.querySelector('.nav-links').classList.toggle('active');
+    let hamburger = document.getElementsByClassName(".hamburger");
+    hamburger.array.forEach(element => {
+        element.style.display = "none";
+    });
 }
