c3e0e4562e5a413e4390723a3f789dc19fbee797
mmill211 <mmill2111@westada.org>
2026-03-24 14:41:03 +0000

    debugged hamburger menu

--- public/navbar.js
+++ public/navbar.js
@@ -1,7 +1,3 @@
 function toggleMenu() {
     document.querySelector('.nav-links').classList.toggle('active');
-    let hamburger = document.getElementsByClassName(".hamburger");
-    hamburger.array.forEach(element => {
-        element.style.display = "none";
-    });
 }
