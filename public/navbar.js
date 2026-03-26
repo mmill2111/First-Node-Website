32c28ee262c6c9a4ad4dfcac420f713659f0f3d9
mmill211 <mmill2111@westada.org>
2026-03-26 15:48:28 +0000

    finished navigation bar

--- public/navbar.js
+++ public/navbar.js
@@ -1,3 +1,27 @@
-function toggleMenu() {
-    document.querySelector('.nav-links').classList.toggle('active');
+function updateStartX() {
+    const sidebarWidth = $("#sidebar").outerWidth();
+    if ($("#sidebar").hasClass("active")) {
+        $("#sidebar").css("transform", "translateX(calc(100vw - " + sidebarWidth + "px))");
+    } else {
+        $("#sidebar").css("transform", "translateX(calc(100vw - 100px))");
+    }
+    console.log($(window).width());
+}
+
+$(".hamburger").click(function(){
+    $("#sidebar").toggleClass("active");
+    updateStartX();
+});
+
+$(document).click(function(arg){
+    if (!$(arg.target).closest("#sidebar").length && !$(arg.target).is(".hamburger")) {
+        $("#sidebar").removeClass("active");
+        updateStartX();
+    } 
+})
+
+window.onresize = function(){
+    if ($(window).width() > 768) {
+        $("#sidebar").removeAttr("style");
+    }
 }
--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const app = express()
  
 const PORT = 3000
 
-app.use(express.static("public"))
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html')
 })
