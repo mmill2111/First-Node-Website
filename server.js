b68bfcf538703de1723bbce9424f8089a405d3ad
mmill211 <mmill2111@westada.org>
2026-04-16 14:39:49 +0000

    added responsive elements to home page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
- 
+const nodemailer = require("nodemailer");
 const app = express()
  
 const PORT = 3000
@@ -7,6 +7,43 @@ const PORT = 3000
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Gmail transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "renctestudent@gmail.com",
+    pass: "qvkutajqvkfrmjkf"
+  }
+});
+
+// Handle form submission
+app.post("/send", async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: "ryan.caufield@infoflo.tech", // must match your Gmail
+      to: "youremail@gmail.com",   // where you receive messages
+      subject: `Form: ${subject}`,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Subject: ${subject}
+        Message: ${message}
+      `
+    });
+
+    res.send("<h2>Email sent successfully!</h2><a href='/'>Go back</a>");
+  } catch (error) {
+    console.error(error);
+    res.send("<h2>Error sending email</h2><a href='/'>Try again</a>");
+  }
+});
+ 
+// Middleware to read form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
